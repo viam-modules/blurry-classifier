522329abca42a705ae9355ec1d771155b6607757
Alan Davidson <alan@viam.com>
2025-02-24 13:40:37 -0500

    debugging

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ cd `dirname $0`
 
 pyinstaller --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz ./dist/main
+# To run locally, we need meta.json in the same directory. So, add to dist/ a
+# symlink that goes one directory up to meta.json
+ln -s ../meta.json dist
