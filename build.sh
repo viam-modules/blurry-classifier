e9b3b23a63142f1f2d947172abd9a63516b7f3c9
Alan Davidson <alan@viam.com>
2025-02-24 13:34:00 -0500

    tweak: use pyinstaller from the local path

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd `dirname $0`
 
 . venv/bin/activate
 
-python -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
+pyinstaller --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz ./dist/main
