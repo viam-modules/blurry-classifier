dc85a968ff7a823e2f63d17a32e3fa34b360613f
Alan Davidson <alan@viam.com>
2025-07-14 16:49:27 -0400

    import the service implementation in main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-# pylint: disable=missing-module-docstring
+# pylint: disable=missing-module-docstring,consider-using-from-import,import-error,unused-import
 import asyncio
 
 from viam.module.module import Module
+import src.blurry_classifier_module
 
 if __name__ == "__main__":
     asyncio.run(Module.run_from_registry())
